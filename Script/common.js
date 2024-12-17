d68f23a0c627e4e0267682e185a28facdabb1480
YousryEssam <youssry.essam2111@gmail.com>
2024-12-17 19:39:14 +0200

    V2.1

--- Script/common.js
+++ Script/common.js
@@ -1,8 +1,4 @@
-const mainProducts = [];
-const productCard = document.querySelector(".card");
-const API_URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
-let productsLimit = 25;
 function goToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
@@ -25,69 +21,28 @@ function goToProductsPage() {
   window.location = "products.html";
 }
 
-//////////////////////
-const MealPrices = [
-  24.39, 32.58, 15.74, 47.12, 12.09, 19.87, 28.42, 40.15, 36.67, 11.39, 25.67,
-  13.99, 45.22, 18.74, 22.18, 30.29, 14.48, 27.96, 39.47, 20.11, 26.83, 48.0,
-  17.61, 44.5, 21.25,
-];
-function createProduct(obj, idx) {
-  return {
-    id: obj["idMeal"],
-    name: obj["strCategory"] + " " + obj["strArea"],
-    imgURL: obj["strMealThumb"],
-    price: MealPrices[idx],
-    description: obj["strIngredient1"] + " " + obj["strIngredient2"],
-  };
-}
-
-function createProductCard(product) {
-  let newProduct = productCard.cloneNode(true);
-  let productIdx = product.idx;
-  let productName = product.name;
-  let productPrice = product.price;
-  let productImgURL = product.imgURL;
-  let productDes = product.description;
-
-  newProduct.querySelector(".card-img").style.backgroundImage =
-    "url('" + productImgURL + "')";
-  newProduct.querySelector(".productPrice").innerHTML = productPrice;
-  newProduct.querySelector(".productName").innerHTML = productName;
-  newProduct.querySelector(".text-body").innerHTML = productDes;
-  newProduct.querySelector(".elementId").innerHTML = String(productIdx);
-  newProduct.style.display = "inline-block";
-  document.getElementById("imgcontainer").appendChild(newProduct);
-}
-
-////
-function getProducts() {
-  fetch(API_URL)
-    .then((respone) => {
-      if (respone.ok) {
-        return respone.json();
-      } else {
-        throw new Error("the data dosent fetches");
-      }
-    })
-    .then((data) => {
-      const limitedMeals = data.meals.slice(0, 25);
-      let idxOfP = 0;
-      limitedMeals.forEach((item) => {
-        mainProducts.push(createProduct(item, idxOfP));
-        idxOfP++;
-      });
-      console.log(mainProducts);
-    })
-    .then(() => {
-      for (let i = 0; i < productsLimit; i++) {
-        createProductCard(mainProducts[i]);
-      }
-    })
-    .catch((error) => {
-      console.error("There was a problem with the fetch operation:", error);
-    });
-}
-
-//////////////
 document.addEventListener("DOMContentLoaded", isUserIn);
-document.addEventListener("DOMContentLoaded", getProducts);
+
+/// Cookies
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
--- Script/products.js
+++ Script/products.js
@@ -1,14 +1,30 @@
 
-let maxNumberOfProducts = 25;
+const mainProducts = [];
+const productCard = document.querySelector(".card");
+const API_URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+let productsLimit = 25;
+const MealPrices = [
+  24.39, 32.58, 15.74, 47.12, 12.09, 19.87, 28.42, 40.15, 36.67, 11.39, 25.67,
+  13.99, 45.22, 18.74, 22.18, 30.29, 14.48, 27.96, 39.47, 20.11, 26.83, 48.0,
+  17.61, 44.5, 21.25,
+];
+function createProduct(obj, idx) {
+  return {
+    id: obj["idMeal"],
+    name: obj["strCategory"] + " " + obj["strArea"],
+    imgURL: obj["strMealThumb"],
+    price: MealPrices[idx],
+    description: obj["strIngredient1"] + " " + obj["strIngredient2"],
+  };
+}
 
-
-function addNewProdunctCard(product, idx) {
+function createProductCard(product) {
   let newProduct = productCard.cloneNode(true);
-  let productIdx = idx;
-  let productImgURL = product["strMealThumb"];
-  let productName = product["strArea"] + " " + product["strCategory"];
-  let productPrice = MealPrices[productIdx];
-  let productDes = product["strIngredient1"] + " " + product["strIngredient2"];
+  let productIdx = product.idx;
+  let productName = product.name;
+  let productPrice = product.price;
+  let productImgURL = product.imgURL;
+  let productDes = product.description;
 
   newProduct.querySelector(".card-img").style.backgroundImage =
     "url('" + productImgURL + "')";
@@ -30,15 +46,22 @@ function getProducts() {
       }
     })
     .then((data) => {
-      const limitedMeals = data.meals.slice(0, maxNumberOfProducts);
+      const limitedMeals = data.meals.slice(0, 25);
       let idxOfP = 0;
       limitedMeals.forEach((item) => {
-        addNewProdunctCard(item, idxOfP);
+        mainProducts.push(createProduct(item, idxOfP));
         idxOfP++;
       });
-    }).catch((error) => {
+      console.log(mainProducts);
+    })
+    .then(() => {
+      for (let i = 0; i < productsLimit; i++) {
+        createProductCard(mainProducts[i]);
+      }
+    })
+    .catch((error) => {
       console.error("There was a problem with the fetch operation:", error);
     });
 }
 
-// document.addEventListener("DOMContentLoaded", getProducts);
+document.addEventListener("DOMContentLoaded", getProducts);
